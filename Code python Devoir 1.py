ca5f02324e61df750f628552276c3c5e8cf80236
AutoKadekuru <jeanzhu2003@gmail.com>
2025-02-15 21:31:25 -0500

    Update Code python Devoir 1.py - Convergence graphs
    Added teacher's code for convergence plots for L1, L2 and Linf error norms for cases C) and E)

--- Code python Devoir 1.py
+++ Code python Devoir 1.py
@@ -48,7 +48,8 @@ def make_b_vector(N, S, D_eff, C_e):
 
 # Constantes
 R = 0.5
-
+N_start = 5
+N_end = 10
 
 S = 2*10**(-8)
 C_e = 20
@@ -61,7 +62,7 @@ L2_2 = []
 Linf_1 = []
 Linf_2 = []
 
-for N in range(5,10) :
+for N in range(N_start, N_end) :
     vect_N.append(R/N)
 
     A_matrix_1 = make_A_matrix_order1(N, 0.5)
@@ -99,6 +100,7 @@ for N in range(5,10) :
    
 
 print(vect_N)
+
 # Affichage des résultats
 plt.figure
 plt.plot(r, C_exact, label="Exacte")
@@ -134,5 +136,261 @@ plt.xlabel("Pas de différentiation (m)")
 plt.legend()
 plt.show()
 
+# Calcul de la convergence du système en prenant N et 2N
+p_hat = (np.log(L1_1[0]/L1_1[4])/np.log(2))
+print("Convergence L1_1 : ", np.log(L1_1[0]/L1_1[4])/np.log(2))
+
+print("Convergence L1_2 : ", np.log(L1_2[0]/L1_2[4])/np.log(2))
+
+# ----- Graphiques de convergences L_1_1
+coefficients = np.polyfit(np.log(vect_N), np.log(L1_1), 1)
+exponent = coefficients[0]
+
+# Fonction de régression en termes de logarithmes
+fit_function_log = lambda x: exponent * x + coefficients[1]
+
+# Fonction de régression en termes originaux
+fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
+
+# Extrapoler la valeur prédite pour la dernière valeur de h_values
+extrapolated_value = fit_function(vect_N[-1])
+
+# Tracer le graphique en échelle log-log avec des points et la courbe de régression extrapolée
+plt.figure(figsize=(8, 6))
+plt.scatter(vect_N, L1_1, marker='o', color='b', label='Données numériques obtenues')
+plt.plot(vect_N, fit_function(vect_N), linestyle='--', color='r', label='Régression en loi de puissance')
+
+# Marquer la valeur extrapolée
+#plt.scatter(h_values[-1], extrapolated_value, marker='x', color='g', label='Extrapolation')
+
+# Ajouter des étiquettes et un titre au graphique
+plt.title('Convergence d\'ordre 1\n de l\'erreur $L_1$ en fonction de $Δx$',
+          fontsize=14, fontweight='bold', y=1.02)  # Le paramètre y règle la position verticale du titre
+
+plt.xlabel('Taille de maille $h_{max}$ ou $Δx$ (cm)', fontsize=12, fontweight='bold')  # Remplacer "h" par "Δx"
+plt.ylabel('Erreur $L_1$ (m/s)', fontsize=12, fontweight='bold')
+
+# Afficher l'équation de la régression en loi de puissance
+equation_text = f'$L_1 = {np.exp(coefficients[1]):.4f} \\times Δx^{{{exponent:.4f}}}$'
+equation_text_obj = plt.text(0.05, 0.05, equation_text, fontsize=12, transform=plt.gca().transAxes, color='k')
+
+# Déplacer la zone de texte
+equation_text_obj.set_position((0.5, 0.4))
 
 
+# ----- Graphiques de convergences L_2_1
+coefficients = np.polyfit(np.log(vect_N), np.log(L2_1), 1)
+exponent = coefficients[0]
+
+# Fonction de régression en termes de logarithmes
+fit_function_log = lambda x: exponent * x + coefficients[1]
+
+# Fonction de régression en termes originaux
+fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
+
+# Extrapoler la valeur prédite pour la dernière valeur de h_values
+extrapolated_value = fit_function(vect_N[-1])
+
+# Tracer le graphique en échelle log-log avec des points et la courbe de régression extrapolée
+plt.figure(figsize=(8, 6))
+plt.scatter(vect_N, L2_1, marker='o', color='b', label='Données numériques obtenues')
+plt.plot(vect_N, fit_function(vect_N), linestyle='--', color='r', label='Régression en loi de puissance')
+
+# Marquer la valeur extrapolée
+#plt.scatter(h_values[-1], extrapolated_value, marker='x', color='g', label='Extrapolation')
+
+# Ajouter des étiquettes et un titre au graphique
+plt.title('Convergence d\'ordre 1\n de l\'erreur $L_2$ en fonction de $Δx$',
+          fontsize=14, fontweight='bold', y=1.02)  # Le paramètre y règle la position verticale du titre
+
+plt.xlabel('Taille de maille $h_{max}$ ou $Δx$ (cm)', fontsize=12, fontweight='bold')  # Remplacer "h" par "Δx"
+plt.ylabel('Erreur $L_2$ (m/s)', fontsize=12, fontweight='bold')
+
+# Afficher l'équation de la régression en loi de puissance
+equation_text = f'$L_2 = {np.exp(coefficients[1]):.4f} \\times Δx^{{{exponent:.4f}}}$'
+equation_text_obj = plt.text(0.05, 0.05, equation_text, fontsize=12, transform=plt.gca().transAxes, color='k')
+
+# Déplacer la zone de texte
+equation_text_obj.set_position((0.5, 0.4))
+
+
+# ----- Graphiques de convergences L_1_2
+coefficients = np.polyfit(np.log(vect_N), np.log(L1_2), 1)
+exponent = coefficients[0]
+
+# Fonction de régression en termes de logarithmes
+fit_function_log = lambda x: exponent * x + coefficients[1]
+
+# Fonction de régression en termes originaux
+fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
+
+# Extrapoler la valeur prédite pour la dernière valeur de h_values
+extrapolated_value = fit_function(vect_N[-1])
+
+# Tracer le graphique en échelle log-log avec des points et la courbe de régression extrapolée
+plt.figure(figsize=(8, 6))
+plt.scatter(vect_N, L1_2, marker='o', color='b', label='Données numériques obtenues')
+plt.plot(vect_N, fit_function(vect_N), linestyle='--', color='r', label='Régression en loi de puissance')
+
+# Marquer la valeur extrapolée
+#plt.scatter(h_values[-1], extrapolated_value, marker='x', color='g', label='Extrapolation')
+
+# Ajouter des étiquettes et un titre au graphique
+plt.title('Convergence d\'ordre 1\n de l\'erreur $L_1$ en fonction de $Δx$',
+          fontsize=14, fontweight='bold', y=1.02)  # Le paramètre y règle la position verticale du titre
+
+plt.xlabel('Taille de maille $h_{max}$ ou $Δx$ (cm)', fontsize=12, fontweight='bold')  # Remplacer "h" par "Δx"
+plt.ylabel('Erreur $L_1$ (m/s)', fontsize=12, fontweight='bold')
+
+# Afficher l'équation de la régression en loi de puissance
+equation_text = f'$L_1 = {np.exp(coefficients[1]):.4f} \\times Δx^{{{exponent:.4f}}}$'
+equation_text_obj = plt.text(0.05, 0.05, equation_text, fontsize=12, transform=plt.gca().transAxes, color='k')
+
+# Déplacer la zone de texte
+equation_text_obj.set_position((0.5, 0.4))
+
+
+# ----- Graphiques de convergences L_2_1
+coefficients = np.polyfit(np.log(vect_N), np.log(L1_2), 1)
+exponent = coefficients[0]
+
+# Fonction de régression en termes de logarithmes
+fit_function_log = lambda x: exponent * x + coefficients[1]
+
+# Fonction de régression en termes originaux
+fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
+
+# Extrapoler la valeur prédite pour la dernière valeur de h_values
+extrapolated_value = fit_function(vect_N[-1])
+
+# Tracer le graphique en échelle log-log avec des points et la courbe de régression extrapolée
+plt.figure(figsize=(8, 6))
+plt.scatter(vect_N, L1_2, marker='o', color='b', label='Données numériques obtenues')
+plt.plot(vect_N, fit_function(vect_N), linestyle='--', color='r', label='Régression en loi de puissance')
+
+# Marquer la valeur extrapolée
+#plt.scatter(h_values[-1], extrapolated_value, marker='x', color='g', label='Extrapolation')
+
+# Ajouter des étiquettes et un titre au graphique
+plt.title('Convergence d\'ordre 2\n de l\'erreur $L_1$ en fonction de $Δx$',
+          fontsize=14, fontweight='bold', y=1.02)  # Le paramètre y règle la position verticale du titre
+
+plt.xlabel('Taille de maille $h_{max}$ ou $Δx$ (cm)', fontsize=12, fontweight='bold')  # Remplacer "h" par "Δx"
+plt.ylabel('Erreur $L_1$ (m/s)', fontsize=12, fontweight='bold')
+
+# Afficher l'équation de la régression en loi de puissance
+equation_text = f'$L_1 = {np.exp(coefficients[1]):.4f} \\times Δx^{{{exponent:.4f}}}$'
+equation_text_obj = plt.text(0.05, 0.05, equation_text, fontsize=12, transform=plt.gca().transAxes, color='k')
+
+# Déplacer la zone de texte
+equation_text_obj.set_position((0.5, 0.4))
+
+
+# ----- Graphiques de convergences L_2_2
+coefficients = np.polyfit(np.log(vect_N), np.log(L2_2), 1)
+exponent = coefficients[0]
+
+# Fonction de régression en termes de logarithmes
+fit_function_log = lambda x: exponent * x + coefficients[1]
+
+# Fonction de régression en termes originaux
+fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
+
+# Extrapoler la valeur prédite pour la dernière valeur de h_values
+extrapolated_value = fit_function(vect_N[-1])
+
+# Tracer le graphique en échelle log-log avec des points et la courbe de régression extrapolée
+plt.figure(figsize=(8, 6))
+plt.scatter(vect_N, L2_2, marker='o', color='b', label='Données numériques obtenues')
+plt.plot(vect_N, fit_function(vect_N), linestyle='--', color='r', label='Régression en loi de puissance')
+
+# Marquer la valeur extrapolée
+#plt.scatter(h_values[-1], extrapolated_value, marker='x', color='g', label='Extrapolation')
+
+# Ajouter des étiquettes et un titre au graphique
+plt.title('Convergence d\'ordre 2\n de l\'erreur $L_2$ en fonction de $Δx$',
+          fontsize=14, fontweight='bold', y=1.02)  # Le paramètre y règle la position verticale du titre
+
+plt.xlabel('Taille de maille $h_{max}$ ou $Δx$ (cm)', fontsize=12, fontweight='bold')  # Remplacer "h" par "Δx"
+plt.ylabel('Erreur $L_2$ (m/s)', fontsize=12, fontweight='bold')
+
+# Afficher l'équation de la régression en loi de puissance
+equation_text = f'$L_2 = {np.exp(coefficients[1]):.4f} \\times Δx^{{{exponent:.4f}}}$'
+equation_text_obj = plt.text(0.05, 0.05, equation_text, fontsize=12, transform=plt.gca().transAxes, color='k')
+
+# Déplacer la zone de texte
+equation_text_obj.set_position((0.5, 0.4))
+
+
+
+# ----- Graphiques de convergences Linf_1
+coefficients = np.polyfit(np.log(vect_N), np.log(Linf_1), 1)
+exponent = coefficients[0]
+
+# Fonction de régression en termes de logarithmes
+fit_function_log = lambda x: exponent * x + coefficients[1]
+
+# Fonction de régression en termes originaux
+fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
+
+# Extrapoler la valeur prédite pour la dernière valeur de h_values
+extrapolated_value = fit_function(vect_N[-1])
+
+# Tracer le graphique en échelle log-log avec des points et la courbe de régression extrapolée
+plt.figure(figsize=(8, 6))
+plt.scatter(vect_N, Linf_1, marker='o', color='b', label='Données numériques obtenues')
+plt.plot(vect_N, fit_function(vect_N), linestyle='--', color='r', label='Régression en loi de puissance')
+
+# Marquer la valeur extrapolée
+#plt.scatter(h_values[-1], extrapolated_value, marker='x', color='g', label='Extrapolation')
+
+# Ajouter des étiquettes et un titre au graphique
+plt.title('Convergence d\'ordre 1\n de l\'erreur $L_inf$ en fonction de $Δx$',
+          fontsize=14, fontweight='bold', y=1.02)  # Le paramètre y règle la position verticale du titre
+
+plt.xlabel('Taille de maille $h_{max}$ ou $Δx$ (cm)', fontsize=12, fontweight='bold')  # Remplacer "h" par "Δx"
+plt.ylabel('Erreur $L_inf$ (m/s)', fontsize=12, fontweight='bold')
+
+# Afficher l'équation de la régression en loi de puissance
+equation_text = f'$L_inf = {np.exp(coefficients[1]):.4f} \\times Δx^{{{exponent:.4f}}}$'
+equation_text_obj = plt.text(0.05, 0.05, equation_text, fontsize=12, transform=plt.gca().transAxes, color='k')
+
+# Déplacer la zone de texte
+equation_text_obj.set_position((0.5, 0.4))
+
+
+
+# ----- Graphiques de convergences Linf_2
+coefficients = np.polyfit(np.log(vect_N), np.log(Linf_2), 1)
+exponent = coefficients[0]
+
+# Fonction de régression en termes de logarithmes
+fit_function_log = lambda x: exponent * x + coefficients[1]
+
+# Fonction de régression en termes originaux
+fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
+
+# Extrapoler la valeur prédite pour la dernière valeur de h_values
+extrapolated_value = fit_function(vect_N[-1])
+
+# Tracer le graphique en échelle log-log avec des points et la courbe de régression extrapolée
+plt.figure(figsize=(8, 6))
+plt.scatter(vect_N, Linf_2, marker='o', color='b', label='Données numériques obtenues')
+plt.plot(vect_N, fit_function(vect_N), linestyle='--', color='r', label='Régression en loi de puissance')
+
+# Marquer la valeur extrapolée
+#plt.scatter(h_values[-1], extrapolated_value, marker='x', color='g', label='Extrapolation')
+
+# Ajouter des étiquettes et un titre au graphique
+plt.title('Convergence d\'ordre 2\n de l\'erreur $L_inf$ en fonction de $Δx$',
+          fontsize=14, fontweight='bold', y=1.02)  # Le paramètre y règle la position verticale du titre
+
+plt.xlabel('Taille de maille $h_{max}$ ou $Δx$ (cm)', fontsize=12, fontweight='bold')  # Remplacer "h" par "Δx"
+plt.ylabel('Erreur $L_inf$ (m/s)', fontsize=12, fontweight='bold')
+
+# Afficher l'équation de la régression en loi de puissance
+equation_text = f'$L_inf = {np.exp(coefficients[1]):.4f} \\times Δx^{{{exponent:.4f}}}$'
+equation_text_obj = plt.text(0.05, 0.05, equation_text, fontsize=12, transform=plt.gca().transAxes, color='k')
+
+# Déplacer la zone de texte
+equation_text_obj.set_position((0.5, 0.4))
